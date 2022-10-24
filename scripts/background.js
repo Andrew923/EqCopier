21932c8b258088fda7ca5b99bdde73ede20a753c
Andrew923 <yuandrew923@gmail.com>
2022-10-23 22:12:28 -0400

    it can take screenshots now

---
+++ scripts/background.js
@@ -0,0 +1,25 @@
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.desktopCapture.chooseDesktopMedia([
+    "screen",
+    "window",
+    "tab"
+    ], tab, (streamId) => {
+    if (streamId && streamId.length) {
+      setTimeout(() => {
+        chrome.tabs.sendMessage(tab.id, {name: "stream", streamId}, (response) => console.log(response))
+      }, 200)
+    }
+  })
+});
+chrome.runtime.onMessage.addListener((message, sender, senderResponse) => {
+  if (message.name === 'download' && message.url) {
+    chrome.downloads.download({
+      filename: 'screenshot.png',
+      url: message.url
+    }, (downloadId) => {
+      senderResponse({success: true})
+    })
+
+    return true;
+  }
+})
